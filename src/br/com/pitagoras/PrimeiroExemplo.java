178f09c1a086f2dd1aa40f0a326a901b9a8a2dcb
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-08-25 21:09:39 -0300

    Inserção de um organizador de layout e um novo componente para exibição de texto.

--- src/br/com/pitagoras/PrimeiroExemplo.java
+++ src/br/com/pitagoras/PrimeiroExemplo.java
@@ -1,12 +1,14 @@
 package br.com.pitagoras;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PrimeiroExemplo {
 
     PrimeiroExemplo() {
         // Criando nosso container (organizador do layout)
         JFrame container = new JFrame("Nosso Primeiro Exemplo");
+        container.setLayout(new FlowLayout());
 
         // Define o tamanho da janela
         container.setSize(300, 100);
@@ -15,9 +17,11 @@ public class PrimeiroExemplo {
 
         // Rótulo exibido na tela.
         JLabel jLabelMsg = new JLabel("Sejam bem vindos à disciplina de POO 2.");
+        JLabel jLabelMsg2 = new JLabel("Segundo rótulo");
 
         // Vincula o componente ao container
         container.add(jLabelMsg);
+        container.add(jLabelMsg2);
 
 //        Define a exibição do container.
         container.setVisible(true);
